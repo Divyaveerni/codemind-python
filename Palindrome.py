750a1eaa1aa04570ccd6f08dd6f57eac27856939
Divyaveerni <83953561+Divyaveerni@users.noreply.github.com>
2022-10-29 14:41:34 +0530

    def palindrome(n):     s=0     t=n     while n>0:         r=n%10         s=s*10+r         n=n//10     if t==s:         print('True')     else:         print('False') n=int(input()) palindrome(n)

--- Palindrome.py
+++ Palindrome.py
@@ -1,13 +1,16 @@
+def palindrome(n):
+    s=0
+    t=n
+    while n>0:
+        r=n%10
+        s=s*10+r
+        n=n//10
+    if t==s:
+        print('True')
+    else:
+        print('False')
 n=int(input())
-s=0
-t=n
-while n>0:
-    r=n%10
-    s=s*10+r
-    n=n//10
-if t==s:
-    print("True")
-else:
-    print("False")
+palindrome(n)
+    
     
     
