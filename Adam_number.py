1aac6ddc7e46810feb05192fa656c0eb6c060c67
Divyaveerni <83953561+Divyaveerni@users.noreply.github.com>
2022-10-29 15:46:33 +0530

    n=int(input()) s1=s=0 t=n while n>0:     k=n%10     s=s*10+k     n=n//10 a=s**2 while a>0:     d=a%10     s1=s1*10+d     a=a//10 if t**2==s1:     print('True') else:     print('False')

--- Adam_number.py
+++ Adam_number.py
@@ -1,17 +1,19 @@
 n=int(input())
+s1=s=0
 t=n
-rev=0
-s=0
 while n>0:
-    r=n%10
-    rev=rev*10+r
-    n=n//10
-m=rev**2
-while m>0:
-    k=m%10
+    k=n%10
     s=s*10+k
-    m=m//10
-if t**2==s:
-    print("True")
+    n=n//10
+a=s**2
+while a>0:
+    d=a%10
+    s1=s1*10+d
+    a=a//10
+if t**2==s1:
+    print('True')
 else:
-    print("False")
+    print('False')
+    
+    
+    
